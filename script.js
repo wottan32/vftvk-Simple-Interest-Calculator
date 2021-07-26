6d8a04c726ed9a64b9eb3e97bddda01a3741f529
Mario Torres <mariotorreslagos@gmail.com>
2021-07-26 14:46:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 function compute() {
+    //Get the values and calculate 
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+    //Create the Interest text
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
+        "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
+        "in the year <mark>" + yearInTheFuture + "</mark>";
 
-    let principal = document.getElementById("amountOne").value 
-    let interestRate = document.getElementById("rate").value
-    let years = document.getElementById("noYears").value
-    let interestEarned = principal * interestRate * years *.01 + Number(principal);// this is still wrong
-    let d = new Date();
-    let n = d.getFullYear();
-    let futureDate = Number(n) + Number(years); 
+}
 
-    document.getElementById("answer1").innerHTML = "If you deposit $" + principal;
-    document.getElementById("answer2").innerHTML = "At a rate of " + interestRate + '%';
-    document.getElementById("answer3").innerHTML = "You'll recieve the amount of $" + interestEarned
-    document.getElementById("answer4").innerHTML = "In the year " + futureDate
-}    
+//update ther ate value
+function getSliderValue() {
+    document.getElementById("rateSpan").innerHTML = document.getElementById("rate").value;
+}
+
+//Check for positive values
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+}
